7f5e69affc18d683728b8995b30ebcf6d91d86f9
Amanda Winkles <amandawinkles@gmail.com>
2020-04-02 16:31:34 -0500

    Complete through indexOf

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,18 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(n === undefined) {
+      return array[array.length - 1];
+    } else if (n === 0) {
+      return [];
+    } else if (n > array.length) {
+      return array;
+    }
+    var lastArr = [];
+    for(var i = array[array.length - (n + 1)]; i < array.length; i++) {
+      lastArr.push(array[i]);
+    }
+    return lastArr;
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +58,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection) === true) {
+      for(var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -58,6 +80,7 @@
     _.each(array, function(item, index) {
       if (item === target && result === -1) {
         result = index;
+        return result;
       }
     });
 
@@ -66,12 +89,27 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passedArray = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        passedArray.push(item);
+      }
+    });
+    return passedArray;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+    var rejectsArray = [];
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var passedArray = _.filter(collection, test);
+    for(var i = 0; i < collection.length; i++) {
+      if(!(passedArray.includes(collection[i]))) {
+        rejectsArray.push(collection[i]);
+      }
+    }
+    return rejectsArray;
   };
 
   // Produce a duplicate-free version of the array.
@@ -107,19 +145,19 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
